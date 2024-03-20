9cbeba510cfb80b1e0c7b760c8e5e16c3971cea5
Ilnur Shafikov <kitatodoshiman@gmail.com>
2024-03-20 10:00:57 +0100

    correction of commentsÐ

--- services/wallet/response/response.go
+++ services/wallet/response/response.go
@@ -5,11 +5,6 @@ type CreateWalletResponse struct {
 	Balance int    `json:"balance"`
 }
 
-type GetBalanceResponse struct {
+type BalanceResponse struct {
 	Balance int `json:"balance"`
-}
-
-type UpdateBalanceResponse struct {
-	UserID  string `json:"user_id"`
-	Balance int    `json:"balance"`
 }
